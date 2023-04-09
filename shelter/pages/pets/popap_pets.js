f779ebb7699c1b740cc39038d5bc18ed43cb2426
Pavel <pashtet.net5@gmail.com>
2023-04-09 21:08:40 +0300

    feat:add modal window and durger menu adaptive for pages pets

---
+++ shelter/pages/pets/popap_pets.js
@@ -0,0 +1,22 @@
+const popap=document.querySelector(".container_modal_window")
+const openPopap=document.querySelectorAll(".img_items_pets" )
+const closePopap=document.querySelector(".header_nav_close")
+
+openPopap.forEach(el => {  
+   el.addEventListener('click', function (e) {  
+        e.target.classList.add('active')
+        showPopup()
+    })           
+})
+async function showPopup() {
+    popap.classList.add('active')
+    const animals = 'animals.json';
+    const res = await fetch(animals );
+    const data = await res.json();
+     document.querySelector(".content_name").textContent=data[0].name                
+    }
+closePopap.addEventListener('click', deletePopup);
+function deletePopup() {
+    popap.classList.remove('active')
+    
+}
